596ea349541abd3194dd89c6ef1a3b1c2d6acbc1
Frankie Christie <lgc8@waikato.ac.nz>
2026-05-01 15:00:56 +1200

    switching from openMapTiles to LINZ. deeper zoom

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let map = L.map("map").setView([0, 0], 1);
 
-L.tileLayer("https://api.maptiler.com/maps/hybrid-v4/{z}/{x}/{y}.jpg?key=fY9qbKMhlDxa9FUU85k9",
-{
-    attribution: `<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> 
-    <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>`
-}).addTo(map);
+L.tileLayer('https://basemaps.linz.govt.nz/v1/tiles/aerial/WebMercatorQuad/{z}/{x}/{y}.webp?api=REMOVED',
+{ maxZoom: 22, attribution: '© LINZ' }
+).addTo(map);
